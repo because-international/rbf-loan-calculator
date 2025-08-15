8c1a3960fd882266ff1789ba098fc63a351356f8
Nate Ritter <hello@nateritter.com>
2025-08-15 16:40:34 -0700

    Exclude .history folder from .gitignore
    Adds the .history folder to the .gitignore file, preventing its files from being tracked.

--- .history/src/index_20250815130038.js
+++
@@ -1,8 +0,0 @@
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import RBFCalculator from './App.js';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <App />
-);
